1e9c8c4f78667270ab3d5c02dcfeff26c602967f
MaxQiu <yueyiqiu178@gmail.com>
2023-11-16 14:31:13 +0800

    加上router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './component/HelloWorld'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <HelloWorld text="今天就開始學React!" />
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter, Route, Routes, Link } from "react-router-dom";
+import HelloWorld from './component/HelloWorld';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+<React.StrictMode>
+    <HashRouter>
+      <nav className="px-5 flex items-center h-[60px] bg-indigo-500 text-white">
+        <h1 className="mr-auto text-2xl">React TodoList</h1>
+        <ul className="flex">
+          <li className="mr-3">
+            <Link to="/" className="border p-3 hover:bg-indigo-600 duration-500">Home</Link>
+          </li>
+          <li>
+            <Link to="/todolist" className="border p-3 hover:bg-indigo-600 duration-500">ToDoList</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={ <App /> } />
+        {<Route path="/HelloWorld" element={ <HelloWorld /> } />}
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 
